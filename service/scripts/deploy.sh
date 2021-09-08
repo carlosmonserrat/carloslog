29d05a0920a1643dbf227804d805e52f90dd6328
Carlos Rojas <c.rojas@portavita.ru>
2021-09-08 17:37:19 +0300

    fix deploy script

--- service/scripts/deploy.sh
+++ service/scripts/deploy.sh
@@ -1,5 +1,6 @@
 SERVER_PASSWORD='9m)L674[Nv}Wt_fp'
 SERVER_URL='202.182.123.158'
+SCRIPTS_FOLDER=$PWD
 cd ..
 sbt docker:publishLocal
 echo "- creating image files"
@@ -8,10 +9,10 @@ echo "- uploading image files"
 scp /tmp/carlosLogService.image "root@${SERVER_URL}:~"
 
 echo "- uploading scripts"
-scp $PWD/load-images.sh "root@${SERVER_URL}:~"
+scp $SCRIPTS_FOLDER/scripts/load-images.sh "root@${SERVER_URL}:~"
 
 echo "- uploading docker files"
-scp $PWD/../deploy/docker-compose.yml "root@${SERVER_URL}:~"
+scp $SCRIPTS_FOLDER/../deploy/docker-compose.yml "root@${SERVER_URL}:~"
 
 echo "- loading images on server"
 sshpass -p ${SERVER_PASSWORD}  ssh root@${SERVER_URL} "echo '${SERVER_PASSWORD}' | /bin/bash load-images.sh"
