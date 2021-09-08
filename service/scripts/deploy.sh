be7978c2c58d56d93e09e9749c37c3db38c90416
Carlos Rojas <c.rojas@portavita.ru>
2021-09-08 17:04:14 +0300

    uncomment deploy

--- service/scripts/deploy.sh
+++ service/scripts/deploy.sh
@@ -1,7 +1,7 @@
 SERVER_PASSWORD='9m)L674[Nv}Wt_fp'
 SERVER_URL='202.182.123.158'
-#cd ..
-#sbt docker:publishLocal
+cd ..
+sbt docker:publishLocal
 echo "- creating image files"
 docker save carlos-log-service:latest -o /tmp/carlosLogService.image
 echo "- uploading image files"
@@ -10,5 +10,9 @@ scp /tmp/carlosLogService.image "root@${SERVER_URL}:~"
 echo "- uploading scripts"
 scp $PWD/load-images.sh "root@${SERVER_URL}:~"
 
+echo "- uploading docker files"
+scp $PWD/../deploy/docker-compose.yml "root@${SERVER_URL}:~"
+
 echo "- loading images on server"
 sshpass -p ${SERVER_PASSWORD}  ssh root@${SERVER_URL} "echo '${SERVER_PASSWORD}' | /bin/bash load-images.sh"
+echo "You can test this service under http://202.182.123.158/ping"
