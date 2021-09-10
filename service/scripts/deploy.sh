c69786138ddf12aae41c7d995b94cfa867dd8cea
Carlos Rojas <c.rojas@portavita.ru>
2021-09-11 01:35:50 +0300

    add database config

---
+++ service/scripts/deploy.sh
@@ -0,0 +1,18 @@
+SERVER_URL='202.182.123.158'
+SCRIPTS_FOLDER=$PWD
+cd ..
+sbt docker:publishLocal
+echo "- creating image files"
+docker save carlos-log-service:latest -o /tmp/carlosLogService.image
+echo "- uploading image files"
+scp /tmp/carlosLogService.image "root@${SERVER_URL}:~"
+
+echo "- uploading scripts"
+scp $SCRIPTS_FOLDER/scripts/load-images.sh "root@${SERVER_URL}:~"
+
+echo "- uploading docker files"
+scp $SCRIPTS_FOLDER/../deploy/docker-compose.yml "root@${SERVER_URL}:~"
+
+echo "- loading images on server"
+sshpass -p ${SERVER_PASSWORD}  ssh root@${SERVER_URL} "echo '${SERVER_PASSWORD}' | /bin/bash load-images.sh"
+echo "You can test this service under http://202.182.123.158/ping"
