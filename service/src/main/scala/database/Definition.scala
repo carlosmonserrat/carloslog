b9a0cb89ecc5a5902eb7a2c7ebf68e805dd44f5c
Carlos Rojas <c.rojas@portavita.ru>
2021-09-20 18:14:29 +0300

    fix database definition, should be singleton

--- service/src/main/scala/database/Definition.scala
+++ service/src/main/scala/database/Definition.scala
@@ -3,15 +3,16 @@ package database
 import scaldi.{Injectable, Injector}
 import slick.basic.DatabaseConfig
 import slick.dbio.{DBIOAction, NoStream}
+import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.api._
-import slick.jdbc.{PostgresProfile, SQLActionBuilder}
 
 import scala.concurrent.Future
 
 class Definition(implicit var injector: Injector) extends Injectable {
+  val db = DatabaseConfig.forConfig[PostgresProfile]("postgres").db
+  implicit val session: Session = db.createSession()
+
   def run[T](queryAction: DBIOAction[T, NoStream, Effect.All]): Future[T] = {
-    DatabaseConfig.forConfig[PostgresProfile]("postgres")
-      .db
-      .run(queryAction)
+    db.run(queryAction)
   }
 }
