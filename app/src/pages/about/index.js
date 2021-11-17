d62e0b0dc401ef29b938eb3ff14080990630adda
Carlos Rojas <c.rojas@portavita.ru>
2021-11-17 15:20:07 +0300

    create docker, add about me page

--- app/src/pages/about/index.js
+++ app/src/pages/about/index.js
@@ -1,7 +1,89 @@
 import React from 'react'
+import Layout from "../../layout";
 
 const About = () => {
-  return (<div>hola</div>)
+  return (<Layout>
+
+    <h1>Hi!</h1>
+    <p>My name is Carlos Monserrat, thanks for the visit. </p>
+    <p>You can take a look to my cv in this link <a href="#">The CV</a>. But you can also enjoy some of my thoughts and
+      words in the follow section.</p>
+    <p>
+      In the last couple of years since 2012 I got the chance to work in the software industry and gain some good
+      experience in the implementation of web API's using different tools and programming languages.
+    </p>
+    <p>
+      Making a career in software engineering was not an easy goal to achieve since most of the educational programs in
+      Bolivia were (probably still are in " "datetime.now()") focus in systems engineering, system administration
+      and some brief introduction to software engineering (which was not clearly distinguish between whats is now the
+      data science career fields and the software management).
+    </p>
+    <p> In any case my motivations were always in trying the different technologies, explore the web!, connect with the
+      world and all the possibilities of being connected to this global network. The first person who introduce me a
+      computer and all the possibilities of this device was my Uncle Limbert.
+      He is a brilliant man from which I was always feeling inspired since a kid, perfect marks in school, outstanding
+      in every field, he started touching the bare metal of this machines as a electronic engineer in his young age and
+      later perform a career in telecommunications, business administration until becoming the great
+      manager he is right now. </p>
+
+    <p>My grand parents brought to him a computer during his studies, a computer with a blue screen (probably a
+      Commodore) and some application called WORK PERFECT to write documents and print them.
+      The printer turns to be an ATARI printer (which probably was not the most successful product), this was not that
+      interesting in those days but at least it was not so hard for myself as a child
+      to adapt to this machine and learn in a short time how to work with it and maintain it, which became a part time
+      job from the times my grand parents needed it for something when my uncle was not there.
+      One evening my uncle brought to our house a Compaq portable computer with a really nice surprise, a brand new
+      video game which everyone was talking about, DOOM!, it was not the first video game I touched since I was already
+      dealing with
+      the old Atari of my uncles but it was still an interesting concept for me to have so much functionalities in a
+      single and portable device!.</p>
+
+    <p>The years pass and my parents realize how important a computer was starting to become for every family, specially
+      in school times, they manage to save some money and finally went with me to the store to pick
+      what was my first desktop computer, I was 11 years old when this happen and became just the beginning of a great
+      way to spent my time.</p>
+
+    <p>In those years I was already starting to introduce myself in the Basic programming language, which was part of
+      some computer educational program at school but unfortunately didn't manage to go deeper
+      since this education was mostly covering some brief chapters to this topics and not really digging in the theory,
+      it was mostly a copy pasting from the teacher's notes in a whiteboard, word by word and carefully
+      in order to successfully make an application run and enjoy your good grade, which for me was something easy to
+      perform, but the true fun was always starting in the experimentation, finishing the redundant exercise and start
+      trying all the different
+      possibilities, patterns, operations and multiple ways to solve the exact same problem, which for the teacher could
+      become a bad thing to do since it would demand from him to start asking to us some more questions
+      and reviewing it more carefully, which for a course of more than 50 students, could take a lot of time, feeling a
+      bit rebel was unavoidable since I was feeling I was reaching some success for each new way of solving a problem I
+      could encounter, but sometimes I could struggle and just feel frustrated of not avoiding the exact same solution
+      as the one written in the boring white board.
+    </p>
+
+    <p>
+      The goal was more or less clear. To finish school and finally start focusing in a way to  understand not just the logic on this thing (which for me was just related to computers),
+      the video games, the internet, all the web sites in those days, the beautiful and amazing animations made in flash,  the people who made this things, and the type of
+      education they also had when making this products. I was not that easy tough.
+    </p>
+    <p>   I finish the school and started a career in Environmental Engineering..., not truly related at all with this current
+      story, but a good reminder of what it means to follow what you feel is correct
+      instead of what others tells you what is better for you, I only study one year of this career to understand that
+      this was not going to make me feel comfortable in a future with a working position</p>
+    <p>
+      It all started in 2012 when finishing the bachelor program in software and systems engineering in La Paz Bolivia,
+      the first time I got the chance to work in a web application was for the publication of the first analysis of the
+      Political Constitution of Bolivia, with simple architecture and design of the annotations, and bunch of plugins
+      for the commentaries and a whole bunch of multiple methods for the proper communication with the database and
+      modules created.
+      For sure was not something beautiful to debug and work around
+      the Scala programming language and frontend
+      client side web applications using some of the, at least in the latest years, Javascript frameworks (React, Vue),
+      which I would not say is the ultimate set of tools, but it has gave a great opportunity to introduce myself in the
+      implementation of some of the latest
+      web application architectures, such as microservices.
+    </p>
+
+    <small>Author: Carlos Andres Monserrat Rojas Rojas</small>
+
+  </Layout>)
 }
 
 export default About
