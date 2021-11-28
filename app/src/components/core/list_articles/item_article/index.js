e21cd13373a1c9e326d974d39d6af1a5d6a28e34
carlos <inf.carlos@outlook.com>
2021-11-28 23:27:13 +0000

    add react replace

--- app/src/components/core/list_articles/item_article/index.js
+++ app/src/components/core/list_articles/item_article/index.js
@@ -3,19 +3,19 @@ import {border, blogPostTitle, blogPostArticle, blogPostLink, blogPostDate, blog
 import {Link} from "react-router-dom";
 
 const ItemArticle = ({article}) => {
-  return (
-    <article className={blogPostArticle}>
-      <Link to={{
-        pathname: "/article",
-        search: `?id=${article.id}`,
-      }} className={blogPostLink}>
-        <div className={border}/>
-        <h1 className={blogPostTitle}>{article.title}</h1>
-        <span className={blogPostDate}>{article.createDate}</span>
-        <p className={blogPostParagraph}>{article.description}</p>
-      </Link>
-    </article>
-  )
+    return (
+        <article className={blogPostArticle}>
+            <Link to={{
+                pathname: "/article",
+                search: `?id=${article.id}`,
+            }} className={blogPostLink}>
+                <div className={border}/>
+                <h1 className={blogPostTitle}>{article.title}</h1>
+                <span className={blogPostDate}>{article.createDate}</span>
+                <p className={blogPostParagraph}>{article.description}</p>
+            </Link>
+        </article>
+    )
 }
 
 export default ItemArticle
